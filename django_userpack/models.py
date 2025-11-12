b06b1854f00c18ece188018188922206bb9090e6
saboalfazl6162 <saboalfazl6162@gmail.com>
2025-11-12 14:28:25 +0330

    add fields

--- django_userpack/models.py
+++ django_userpack/models.py
@@ -2,6 +2,23 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class AdvancedUser(AbstractUser):
+    # Auth fields
     phone_number = models.CharField()
     national_code = models.CharField()
+
+
     
+    # Personal information
+    
+    ## times and numbers
+    
+    birth_day = models.DateTimeField()
+    verify_date = models.DateTimeField()
+    age = models.PositiveSmallIntegerField()
+
+    ## permissions
+    is_verify = models.BooleanField()
+
+    ## more
+    bio = models.TextField()
+    avatar = models.ImageField()
